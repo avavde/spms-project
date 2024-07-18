178788e872e12a52c3a1081220e6afb8f74791f7
Aleksei Avdeev <37865359+avavde@users.noreply.github.com>
2024-07-18 22:02:28 +0200

    Create BeaconFloorPlan.js

---
+++ backend/models/BeaconFloorPlan.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const Building = require('./Building');
+const FloorPlan = require('./FloorPlan');
+
+class BeaconFloorPlan extends Model {}
+
+BeaconFloorPlan.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  building_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Building,
+      key: 'id',
+    },
+  },
+  floorplan_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: FloorPlan,
+      key: 'id',
+    },
+  },
+  // другие поля
+}, {
+  sequelize,
+  modelName: 'BeaconFloorPlan',
+  tableName: 'beacon_floor_plans',
+  timestamps: false,
+});
+
+module.exports = BeaconFloorPlan;
