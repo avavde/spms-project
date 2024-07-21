31cd9e000a11923c3eadbcce78cc265d63b19629
Aleksei Avdeev <37865359+avavde@users.noreply.github.com>
2024-07-21 12:36:06 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Логирование всех запросов
 app.use((req, res, next) => {
-  console.log(` ${req.method} ${req.url} ${req.body} `);
+  console.log(`Method: ${req.method}, URL: ${req.url}, Headers: ${JSON.stringify(req.headers)}, Body: ${JSON.stringify(req.body)}`);
   next();
 });
 
