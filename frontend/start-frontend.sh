3ee163aaaab5cce67220c42d526810a98228928f
Aleksei Avdeev <37865359+avavde@users.noreply.github.com>
2024-07-24 09:11:06 +0200

    Create start-frontend.sh

---
+++ frontend/start-frontend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Остановка скрипта при ошибке
+set -e
+
+
+cd /home/spms-project/frontend
+# Установка зависимостей
+
+
+# Запуск проекта
+npm start
+
+
+echo "Frontend project is running successfully"
